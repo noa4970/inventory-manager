c2d7872dd8c1622d5b4320d5334e6bc939dfb2d3
Noa Peles <noapeles@MacBookAir.lan>
2025-07-17 09:21:21 +0300

    Organize project: add backend and frontend apps

---
+++ backend/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3;
+            n1 = int.Parse(Console.ReadLine());
+            n2 = int.Parse(Console.ReadLine());
+            n3 = int.Parse(Console.ReadLine());
+            if (n1 > n2)
+            {
+                if (n1 > n3)
+                    Console.WriteLine(n1);
+                else
+                    Console.WriteLine(n3);
+            }
+            else
+            {
+                if (n2 > n3)
+                    Console.WriteLine(n2);
+                else
+                    Console.WriteLine(n3);
+            }
+            Console.ReadLine();
+        }
+    }
+}
